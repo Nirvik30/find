a22db432912e00c4bca879d8af7333ee8157f012
Nirvik30 <nirvikacharekar@gmail.com>
2025-07-22 13:11:37 +0530

    gsefg

--- client/src/pages/applicant/JobAlerts.tsx
+++ client/src/pages/applicant/JobAlerts.tsx
@@ -8,7 +8,7 @@ import {
   CardHeader,
   CardTitle,
   CardFooter,
-} from '@/components/ui/card';
+} from '@/components/ui/card';   
 import { Badge } from '@/components/ui/badge';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
